ab2af2c45ba163d9a3528600d6900cf0562e7db7
wqsz7xn <wqsz7xn@protonmail.com>
2020-07-13 01:46:08 +1200

    xmonad updates

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,21 +1,41 @@
-import XMonad
-import XMonad.Hooks.DynamicLog
-import XMonad.Hooks.ManageDocks
-import XMonad.Util.Run(spawnPipe)
-import XMonad.Util.EZConfig(additionalKeys)
-import System.IO
+import           Graphics.X11.ExtraTypes.XF86
+import           System.Exit
+import           System.IO
+import           XMonad
+import           XMonad.Hooks.DynamicLog
+import           XMonad.Hooks.ManageDocks
+import           XMonad.Hooks.Script
+import           XMonad.Prompt
+import           XMonad.Prompt.Shell
+import           XMonad.Prompt.XMonad
+import           XMonad.Util.Dmenu
+import           XMonad.Util.EZConfig         (additionalKeys)
+import           XMonad.Util.Run              (spawnPipe)
 
-main = do   
+main = do
     xmproc <- spawnPipe "xmobar"
     xmonad $ docks defaultConfig
-        { layoutHook = avoidStruts  $  layoutHook defaultConfig
+        { layoutHook = avoidStruts $ layoutHook defaultConfig
         , logHook = dynamicLogWithPP xmobarPP
                         { ppOutput = hPutStrLn xmproc
                         , ppTitle = xmobarColor "green" "" . shorten 50
                         }
         , modMask = mod4Mask
         } `additionalKeys`
-        [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
-        , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
-        , ((0, xK_Print), spawn "scrot")
+        [ ((mod4Mask, xK_a ), spawn "scrot -s")
+        , ((mod4Mask, xK_l ), spawn "sleep 1; xset dpms force off; slock")
+        , ((mod4Mask, xK_r ), spawn "autorandr --change")
+        , ((0, xF86XK_AudioLowerVolume   ), spawn "amixer set Master 2-")
+        , ((0, xF86XK_AudioRaiseVolume   ), spawn "amixer set Master 2+")
+        , ((0, xF86XK_AudioMute          ), spawn "amixer set Master toggle")
+        , ((mod4Mask, xK_z),
+            xmonadPromptC systemPromptCmds defaultXPConfig)
         ]
+
+systemPromptCmds = [
+        ("shutdown", spawn "shutdown now"),
+        ("reboot", spawn "reboot"),
+        ("exit", io $ exitWith ExitSuccess),
+        ("lock", spawn "sleep 1; xset dpms force off; slock"),
+        ("restart", restart "xmonad" True)
+    ]
