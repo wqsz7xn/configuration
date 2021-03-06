dc4204d27cb24119f2bc5bbc0d8b0170a0a7f58f
wqsz7xn <wqsz7xn@protonmail.com>
2021-03-06 22:28:43 +1300

    update

---
+++ .emacs.d/init.el
@@ -0,0 +1,52 @@
+;; init
+(require 'package)
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+;; basic
+(setq inhibit-startup-message t)
+(setq visible-bell t)
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(tooltip-mode -1)
+(set-fringe-mode 10)
+
+;; numbers
+(column-number-mode)
+(global-display-line-numbers-mode t)
+
+;; packages
+(use-package magit
+  :commands magit-status)
+
+(use-package dired
+  :ensure nil)
+
+(use-package general)
+
+(use-package which-key
+  :defer 0
+  :diminish which-key-mode
+  :config
+  (which-key-mode)
+  (setq which-key-idle-delay 1))
+
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+;; theme
+(use-package grandshell-theme
+  :init (load-theme 'grandshell t))
+
--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -18,21 +18,16 @@ main = do
         { layoutHook = avoidStruts $ layoutHook defaultConfig
         , logHook = dynamicLogWithPP xmobarPP
                         { ppOutput = hPutStrLn xmproc
-                        , ppTitle = xmobarColor "green" "" . shorten 20
+                        , ppTitle = xmobarColor "red" "" . shorten 20
                         }
         , modMask = mod4Mask
-        , focusedBorderColor = "#FFFF00"
-	, borderWidth = 1
+        , borderWidth = 1
         } `additionalKeys`
-        [ ((mod4Mask, xK_a ), spawn "scrot -s")
-        , ((mod4Mask, xK_r ), spawn "autorandr --change")
+        [ ((mod4Mask, xK_s ), spawn "sleep 0.2; scrot ~/Pictures/Screenshots/%b%d::%H%M%S.png -s")
         , ((mod4Mask, xK_e ), spawn "emacs")
-        , ((mod4Mask, xK_f ), spawn "firefox")
+        , ((mod4Mask, xK_f ), spawn "chromium")
+        , ((mod4Mask, xK_b ), spawn "blueman-manager")
         , ((mod4Mask, xK_m ), spawn "thunderbird")
-        , ((0, xF86XK_AudioLowerVolume   ), spawn "amixer set Master 2-")
-        , ((0, xF86XK_AudioRaiseVolume   ), spawn "amixer set Master 2+")
-        , ((0, xF86XK_AudioMute          ), spawn "amixer -D pulse set Master Playback Switch toggle")
-        , ((0, xF86XK_AudioMicMute), spawn "amixer set Capture toggle")
         , ((mod4Mask, xK_z),
             xmonadPromptC systemPromptCmds defaultXPConfig)
         ]
@@ -41,6 +36,8 @@ systemPromptCmds = [
         ("shutdown", spawn "shutdown now"),
         ("reboot", spawn "reboot"),
         ("exit", io $ exitWith ExitSuccess),
+        ("suspend", io $ spawn "systemctl suspend"),
         ("lock", spawn "sleep 1; xset dpms force off; slock"),
-        ("restart", restart "xmonad" True)
+        ("restart (xmonad)", restart "xmonad" True)
     ]
+
