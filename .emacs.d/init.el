63917b93548423f900d682257898443cfdd374c0
wqsz7xn <wqsz7xn@protonmail.com>
2021-03-07 21:09:34 +1300

    ivy/swiper/counsel as completion for minibuffer

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -28,6 +28,36 @@
 (global-display-line-numbers-mode t)
 
 ;; packages
+
+(use-package ivy
+  :diminish
+  :defer 0.1
+  :bind (("C-c C-r" . ivy-resume)
+         ("C-x B" . ivy-switch-buffer-other-window))
+  :custom
+  (ivy-count-format "(%d/%d) ")
+  (ivy-use-virtual-buffers t)
+  :config (ivy-mode))
+
+(use-package ivy-rich
+  :after ivy
+  :custom
+  (ivy-virtual-abbreviate 'full
+                          ivy-rich-switch-buffer-align-virtual-buffer t
+                          ivy-rich-path-style 'abbrev)
+  :config
+  (ivy-set-display-transformer 'ivy-switch-buffer
+                               'ivy-rich-switch-buffer-transformer))
+
+(use-package swiper
+  :after ivy
+  :bind (("C-s" . swiper)
+         ("C-r" . swiper)))
+
+(use-package counsel
+  :after ivy
+  :config (counsel-mode))
+
 (use-package magit
   :commands magit-status)
 
@@ -46,7 +76,30 @@
 (use-package rainbow-delimiters
   :hook (prog-mode . rainbow-delimiters-mode))
 
-;; theme
-(use-package grandshell-theme
-  :init (load-theme 'grandshell t))
+(use-package lsp-mode
+  :commands (lsp lsp-deferred)
+  :init
+  (setq lsp-keymap-prefix "C-c l")
+  :config
+  (lsp-enable-which-key-integration t))
 
+(use-package lsp-ui
+  :hook (lsp-mode . lsp-ui-mode))
+
+(use-package company
+  :after lsp-mode
+  :hook (prog-mode . company-mode)
+  :bind (:map company-active-map
+         ("<tab>" . company-complete-selection))
+        (:map lsp-mode-map
+         ("<tab>" . company-indent-or-complete-common))
+  :custom
+  (company-minimum-prefix-length 1)
+  (company-idle-delay 0.0))
+
+;; haskell
+(use-package haskell-mode)
+
+(use-package lsp-haskell
+  :hook ((haskell-mode . lsp)
+         (haskell-literate-mode . lsp)))
