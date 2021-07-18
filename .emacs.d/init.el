3baa977ac234785de5a16a5c993e17f6f2cdebce
wqsz7xn <wqsz7xn@protonmail.com>
2021-07-19 11:28:25 +1200

    keep folders clean w/ no-litter

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -159,6 +159,14 @@
   :config
   (evil-collection-init))
 
+;; clean folders
+(use-package no-littering)
+
+;; no-littering doesn't set this by default so we must place
+;; auto save files in the same path as it uses for sessions
+(setq auto-save-file-name-transforms
+            `((".*" ,(no-littering-expand-var-file-name "auto-save/") t)))
+
 ;; haskell
 (use-package haskell-mode)
 
